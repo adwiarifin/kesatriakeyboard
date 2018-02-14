5adc6087b8ae227b6a86cb123ed65f6e41bddb75
Adwi Arifin <adwiarifin@gmail.com>
2018-02-15 00:41:30 +0700

    make migration

---
+++ database/migrations/2018_02_14_172932_create_blog_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('blog_post_tag', function (Blueprint $table) {
+            $table->integer('post_id')->references('id')->on('blog_posts');
+            $table->integer('tag_id')->references('id')->on('blog_tags');
+            $table->primary(['post_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_tags');
+    }
+}
