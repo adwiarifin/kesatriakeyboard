d312532a7653138eff6a2d51048c175a02a4b1af
Adwi Arifin <adwiarifin@gmail.com>
2018-02-16 18:14:37 +0700

    remove commented tag in blogpost

--- resources/views/front/blogposts.blade.php
+++ resources/views/front/blogposts.blade.php
@@ -59,62 +59,6 @@
                                 @php $odd = !$odd @endphp
                             </div>
                             @endforeach
-
-                            <!--div class="card card-plain card-blog">
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h6 class="card-category text-danger">
-                                                <i class="fa fa-free-code-camp" aria-hidden="true"></i> Trending
-                                            </h6>
-                                            <h3 class="card-title">
-                                                <a href="#pablo">Uber acqui-hires social app studio Swipe Labs</a>
-                                            </h3>
-                                            <p class="card-description">
-                                                These issues might be making it a bit harder for Uber to hire right now in the competitive Silicon Valley job market. Acqui-hiring companies like it’s doing here with Swipe Labs lets it roll up a bunch of good talent — and… <a href="#pablo"> Read More </a>
-                                            </p>
-                                            <p class="author">
-                                                by <a href="#pablo"><b>Josh Constine</b></a>, 2 days ago
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="card-image">
-                                            <a href="#pablo">
-                                                <img class="img" src="{{ url('img/blog2.jpg') }}" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card card-plain card-blog">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="card-image">
-                                            <a href="#pablo">
-                                                <img class="img" src="{{ url('img/blog3.jpg') }}" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h6 class="card-category text-success">
-                                                Startups
-                                            </h6>
-                                            <h3 class="card-title">
-                                                <a href="#pablo">HBO and Netflix lead this year’s nominations</a>
-                                            </h3>
-                                            <p class="card-description">
-                                                Streaming services once again top the list of this year’s Emmy nominations – another indicator of the shift in how today’s consumers are watching TV. HBO, which has been available. <a href="#pablo"> Read More </a>
-                                            </p>
-                                            <p class="author">
-                                                by <a href="#pablo"><b>Sarah Perez</b></a>, 14 Jul 2017
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div-->
                         </div>
                     </div>
                 </div>
