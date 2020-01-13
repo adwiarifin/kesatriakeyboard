87ef953a7cac313d542bde8317279fd036006376
Adwi Arifin <adwi@itmi.id>
2020-01-13 12:02:11 +0700

    modify travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Deploying stage kesatriakeyboard"
+ssh -p 34512 -i ./deploy_key kesatria@hamzah.hideserver.net
+
+cd /home/kesatria/kesatriakeyboard \
+&& git checkout master \
+&& git pull \
+&& composer install
+&& echo "kesatriakeyboard deployed successfully"
